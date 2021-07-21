b56d89db549220b8c4124402ddcf8ef98a8fcf87
Ran05 <ferarizarandolfh.ifliba@gmail.com>
2021-07-21 17:36:06 +0800

    adding templates
    adding-username 22:10

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/img/logo.png" class="w-100 rounded-circle">
+        </div>
+        <div class="col-9 p-5">
+            <div class="d-flex align-items-center pb-4">
+                <h1>Developer Mode</h1>
             </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>127k</strong>post</div>
+                <div class="pr-5"><strong>24k</strong>followers</div>
+                <div class="pr-5"><strong>300</strong>following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">Developer Mode</div>
+            <div> This is a sample description</div>
+            <div> <a href="#">developermode.com</a></div>
+        </div>
+    </div>
+    <!-- Here is another row -->
+    <div class="row">
+        <div class="col-4">
+            <img src="/img/post-1.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/img/post-2.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/img/post-3.jpg" class="w-100">
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,10 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <!-- {{ config('app.name', 'Laravel') }} -->
+                    <div><img src="/img/logo.png" style="height: 35px; border-right: 1px solid #333" class="pr-2"></div>
+                    <div class="pl-2"> Developer Mode</div> 
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
